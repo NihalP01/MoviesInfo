c460c73b7bea3964916f8df092811defc9ae8e28
NihalP01 <nihalburagohain02@gmail.com>
2020-07-13 17:56:27 +0530

    Minor Changes to the UI

--- app/src/main/java/com/nihalp01/movies/Network/API/TmdbEndpoints.kt
+++ app/src/main/java/com/nihalp01/movies/Network/API/TmdbEndpoints.kt
@@ -1,11 +1,16 @@
 package com.nihalp01.movies.Network.API
 
-import com.nihalp01.movies.Network.API.PopularMovies
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface TmdbEndpoints {
     @GET("/3/movie/popular")
-    fun getMovies(@Query("api_key") key: String): Call<PopularMovies>
+    fun getMovies(@Query("api_key") key: String): Call<Movies>
+    @GET("/3/movie/top_rated")
+    fun getTopRated(@Query("api_key") key: String): Call<Movies>
+    @GET("/3/movie/latest")
+    fun getLatest(@Query("api_key") key: String) : Call<Movies>
+    @GET("/3/movie/upcoming")
+    fun getUpcoming(@Query("api_key") key: String) : Call<Movies>
 }
--- app/src/main/java/com/nihalp01/movies/UI/Fragment/FragmentTopRated.kt
+++ app/src/main/java/com/nihalp01/movies/UI/Fragment/FragmentTopRated.kt
@@ -4,15 +4,47 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.nihalp01.movies.Network.API.Movies
+import com.nihalp01.movies.Network.API.TmdbEndpoints
+import com.nihalp01.movies.Network.ServiceBuilder
 import com.nihalp01.movies.R
+import com.nihalp01.movies.UI.MoviesAdapter
+import kotlinx.android.synthetic.main.fragment_top_rated.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class FragmentTopRated: Fragment() {
+class FragmentTopRated : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_top_rated, container, false)
+        val view = inflater.inflate(R.layout.fragment_top_rated, container, false)
+        val request = ServiceBuilder.buildService(TmdbEndpoints::class.java)
+        val call = request.getTopRated(getString(R.string.api_key))
+
+        call.enqueue(object : Callback<Movies> {
+            override fun onFailure(call: Call<Movies>, t: Throwable) {
+                Toast.makeText(context, "${t.message}", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onResponse(call: Call<Movies>, response: Response<Movies>) {
+                if (response.isSuccessful) {
+                    my_progressbar_toprated?.visibility = View.GONE
+                    recyclerView_top_rated?.apply {
+                        setHasFixedSize(true)
+                        adapter = MoviesAdapter(context, response.body()!!.results)
+                        layoutManager = LinearLayoutManager(context)
+                    }
+                }
+            }
+
+        })
+        return view
     }
+
 }
